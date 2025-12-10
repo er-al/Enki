8ddd4e1a5374000f2316e9474f4914c807ee40a1
Eric Almendral <eralmndrx@gmail.com>
2025-12-10 11:19:10 +0800

    app: typing WIP

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,13 @@
 import type React from 'react';
 import './App.css';
+import TypingTest from './components/TypingTest';
 
 const App: React.FC = () => {
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <h1>Speed Typing Test</h1>
+      <p>Test your typing speed! Type the words below as fast as you can.</p>
+      <TypingTest />
     </div>
   );
 };
